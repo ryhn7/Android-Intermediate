0611ea12bbc74a3544c030a7af79e985b54d29bb
Rayhan Illyas <rayhan205732@gmail.com>
2023-04-30 19:39:54 +0700

    Android - Service

---
+++ Service/app/src/main/java/com/example/myservice/MyForegroundService.kt
@@ -0,0 +1,84 @@
+package com.example.myservice
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import kotlinx.coroutines.*
+
+class MyForegroundService : Service() {
+
+    companion object{
+        private  const val NOTIFICATION_ID = 1
+        private const val CHANNEL_ID = "channel_01"
+        private const val CHANNEL_NAME = "Han Service Channel"
+        internal val TAG = MyForegroundService::class.java.simpleName
+    }
+
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+    private fun buildNotification(): Notification{
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingFlags: Int = if (Build.VERSION.SDK_INT >= 23) {
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        } else {
+            PendingIntent.FLAG_UPDATE_CURRENT
+        }
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, pendingFlags)
+
+        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Han Service")
+            .setContentText("Han Service Running")
+            .setSubText("Han Service")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentIntent(pendingIntent)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = CHANNEL_NAME
+            notificationBuilder.setChannelId(CHANNEL_ID)
+            mNotificationManager.createNotificationChannel(channel)
+        }
+
+        return notificationBuilder.build()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val notification = buildNotification()
+
+        startForeground(NOTIFICATION_ID, notification)
+
+        Log.d(TAG, "Service dijalankan.... ")
+
+        serviceScope.launch {
+            for (i in 1..20){
+                delay(1000)
+                Log.d(TAG, "Do Something: $i")
+            }
+            stopForeground(true)
+            stopSelf()
+            Log.d(TAG, "Service dihentikan")
+        }
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceJob.cancel()
+        Log.d(TAG, "onDestroy: Service dihentikan")
+    }
+}
